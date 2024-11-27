99749c7b9fe8e902427f3d16638b750da18fcfc3
nilver987 <nilver.1967@gmail.com>
2024-11-27 12:37:39 -0500

    Primer commit

--- ms-producto/src/main/java/com/example/msproducto/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.msproducto.repository;
-
-import com.example.msproducto.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
